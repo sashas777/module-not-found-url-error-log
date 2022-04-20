d836669677ddd840b602f5d7e1fea634166108b7
Alexander Lukyanov <cybergomel@gmail.com>
2022-04-20 04:02:44 -0400

    Magento 2.4.4 PHP 8.1.x

--- registration.php
+++ registration.php
@@ -4,6 +4,8 @@
  * @copyright  2022  Sashas IT Support Inc. (https://www.sashas.org)
  * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
  */
+declare(strict_types=1);
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'TheSGroup_NotFoundUrlLog', __DIR__);
